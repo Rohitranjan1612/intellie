b92e44c57f7e8acea2a422ad773136d85d9c650d
Rohit Ranjan <rohitranjan1612@gmail.com>
2023-10-01 00:06:51 +0530

    move code

--- App.tsx
+++ App.tsx
@@ -1,31 +1,8 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import StackScreens from './Navigation/StackNavigation';
 
 function App(): JSX.Element {
-  return (
-    <View>
-      <Text>Hi</Text>
-    </View>
-  );
+  return <StackScreens />;
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
